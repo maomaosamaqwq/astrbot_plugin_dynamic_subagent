9a6dc2ad7524cc2052e12da5a1aefd120ba47006
maomaosamaqwq <maomaosamaqwq@example.com>
2026-05-30 00:16:57 +0800

    chore: cleanup check scripts

--- push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d D:\aibot\openclaw\workspace\astrbot_plugin_dynamic_subagent
-git add -A
-git commit -m "fix: re-translate all files to Dynamic SubAgent content"
-git push origin master
